97b5ca6a60084e07ad01106078888f707bbd52ac
penadidik <di2k.skh@gmail.com>
2021-08-07 07:20:01 +0700

    lifecycle activity

--- app/src/main/java/info/penadidik/youtubecontent/MainActivity.kt
+++ app/src/main/java/info/penadidik/youtubecontent/MainActivity.kt
@@ -2,10 +2,31 @@ package info.penadidik.youtubecontent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "Siklus hidup onStart!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "Siklus hidup onPause!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "Siklus hidup onResume!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Siklus hidup onDestroy!", Toast.LENGTH_SHORT).show()
     }
 }
